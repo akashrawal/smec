9c9779203abd703abd1d23818d8be57fe0491e67
Akash Rawal <argmlac@gmail.com>
2016-08-11 13:19:14 +0530

    Initial commit

---
+++ sme/incl.h
@@ -0,0 +1,33 @@
+/* incl.h
+ * Includes
+ * 
+ * Copyright 2015 Akash Rawal
+ * This file is part of Modular Middleware.
+ * 
+ * Modular Middleware is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Modular Middleware is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Modular Middleware.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+//TODO: Decide the includes in API headers
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <sys/types.h>
+
+#include <mmc/mmc.h>
+#include <ssc/ssc.h>
+
+//Include all modules in dependency-based order
+
+
---
+++ sme/sme.h
@@ -0,0 +1,26 @@
+/* sme.h
+ * API header file
+ * 
+ * Copyright 2015 Akash Rawal
+ * This file is part of Modular Middleware.
+ * 
+ * Modular Middleware is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Modular Middleware is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Modular Middleware.  If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
+#ifndef SME_H_INCLUDED
+#define SME_H_INCLUDED
+
+#include "incl.h"
+
+#endif //SME_H_INCLUDED
